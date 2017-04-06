b5040909a3e9a459ec8b310eb2282750d5d6cf85
Darryl Pogue <darryl@ayogo.com>
2017-04-06 14:28:24 -0700

    Report milliseconds back to Chrome >=58

--- lib/rails_server_timings/controller_runtime.rb
+++ lib/rails_server_timings/controller_runtime.rb
@@ -6,13 +6,22 @@ module RailsServerTimings
         payload = event.payload
         timings = []
 
+        factor = 1.0
+
+        # Chrome 57 and lower interpret the header in seconds instead of
+        # milliseconds, so we need to divide all the results by a factor of
+        # 1000
+        if match = request.user_agent.match(/Chrome\/(\d+)/)
+          factor = 0.0001 if match[1] and match[1].to_i < 58
+        end
+
         payload.each do |key, value|
           if idx = key.to_s =~ /_runtime/
-            timings << ("#{key[0, idx]}=%.3f" % (value.to_f/1000.0))
+            timings << ("#{key[0, idx]}=%.3f" % (value.to_f * factor))
           end
         end
 
-        timings << ('total=%.3f' % (event.duration.to_f/1000.0))
+        timings << ('total=%.3f' % (event.duration.to_f * factor))
 
         response.headers['Server-Timing'] = timings.join(', ')
       end
